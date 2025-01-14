2f633919c0c3da11821de33940709805b5c6e2b2
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-13 23:02:16 -0800

    Create firstContract.sol

---
+++ solidity-exercises/contracts/firstContract.sol
@@ -0,0 +1,20 @@
+
+
+
+contract FirstContract {
+
+    function helloFunction()
+        public
+        pure 
+        returns (uint256) {
+            return 100;
+        }
+
+    function otherFunction()
+        public 
+        pure 
+        returns (bool) {
+            return true;
+        }
+
+}
