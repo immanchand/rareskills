708d31a6fdb7d78812b7590d7727e9e784ca56a5
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-13 22:31:56 -0800

    Update Add.sol

--- solidity-exercises/contracts/Add.sol
+++ solidity-exercises/contracts/Add.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Add{
+    function add(uint256 x, uint256 y) public pure returns(uint256){
+        uint256 sum = x + y;
+        return sum;
+    }
+}
