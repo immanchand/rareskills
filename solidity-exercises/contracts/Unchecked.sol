d5eaacd387c69bf26dfd4cfb09bcf155f46cf018
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-14 01:09:29 -0800

    Create Unchecked.sol

---
+++ solidity-exercises/contracts/Unchecked.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Unchecked {
+    /*
+        This exercise assumes you understand what unchecked keyword is.
+        1. The `getNumber` function reverts when called, you need to make the function stop
+           reverting and return underflow value.
+    */
+     
+
+    function getNumber(uint256 x) public pure returns (uint256) { // Corrected syntax
+            return x - 100;
+        
+    }
+
+
+   
+}
