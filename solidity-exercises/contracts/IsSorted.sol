1b736201ae4a22901ce806685b694e76faf599dc
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-19 02:26:02 -0800

    Create IsSorted.sol

---
+++ solidity-exercises/contracts/IsSorted.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+contract IsSorted {
+    /**
+     * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
+     */
+    function isSorted(uint256[] calldata arr) public view returns (bool) {
+        // your code here
+
+        for(uint256 i=0; i<(arr.length-1);i++){
+
+            if(arr[i]>arr[i+1])
+            return false;
+
+        }
+        return true;
+    }
+}
