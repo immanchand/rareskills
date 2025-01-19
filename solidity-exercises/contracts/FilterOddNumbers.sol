b2f014c711bfd5f2d6dd357ddce73c7471b299c7
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-19 02:09:03 -0800

    Create FilterOddNumbers.sol

---
+++ solidity-exercises/contracts/FilterOddNumbers.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+contract FilterOddNumbers {
+    /*
+        This exercise assumes you understand how to manipulate Array.
+        1. Function `filterOdd` takes an array of uint256 as argument. 
+        2. Filter and return an array with the odd numbers removed.
+        Note: this is tricky because you cannot allocate a dynamic array in memory, 
+              you need to count the even numbers then declare an array of that size.
+    */
+
+    function filterOdd(uint256[] memory _arr)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 lengthCount =0;
+        
+        for(uint256 i=0;i<_arr.length;i++){
+            if(_arr[i]%2==0)
+            lengthCount++;
+        }
+        uint256[] memory evenNumbers = new uint256[](lengthCount);
+        uint256 eveni = 0;
+
+        for(uint256 i=0;i<_arr.length;i++){
+            if(_arr[i]%2==0){
+                evenNumbers[eveni] = _arr[i];
+                eveni++;
+            }       
+        }
+        return evenNumbers;
+    }
+}
