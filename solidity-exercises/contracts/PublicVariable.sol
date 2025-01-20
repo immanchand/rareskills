417e2459ab4722ac241229fea36a6ce6f3c82eea
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-20 00:48:57 -0800

    Create PublicVariable.sol

---
+++ solidity-exercises/contracts/PublicVariable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract PublicVariable {
+    // change this code so this number
+    // is visible outside the contract
+    uint256 public number = 42;
+
+
+}
