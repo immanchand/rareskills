810bc9ef0a6af1186e349cd94d4ea7fdd03326ff
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-20 22:34:06 -0800

    Create Stack.sol

---
+++ solidity-exercises/contracts/Stack.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Stack {
+    /*
+        This exercise assumes you understand what a stack is and know how to manipulate it.
+        1. Implement a stack using an array `stack` with the following functions:
+            a. `push` function appends a new value to the end of the array.
+            b. `peek` function returns the last element of the array without removing it, 
+               but first checks if the stack is not empty.
+            c. `pop` function returns the last element of the array and removes it from 
+               the stack, but also checks if the stack is not empty.
+            d. `size` function returns the length of the array, which corresponds to
+               the number of elements in the stack.
+            e. `getStack` function returns the stack.
+    */
+
+    uint256[] stack;
+
+    constructor(uint256[] memory _stack) {
+        stack = _stack;
+    }
+    function push(uint256 newValue) public{
+        stack.push(newValue);
+    }
+    function peek() public view returns(uint256 lastElement){
+        if(stack.length > 0)
+        return stack[stack.length-1];
+    }
+    function pop() public returns(uint256 lastElement){
+        if(stack.length > 0){
+        lastElement = stack[stack.length-1];
+        stack.pop();
+        return lastElement;
+        }
+    }
+    function size() public view returns(uint256 length){
+        return stack.length;
+    }
+    function getStack() public view returns(uint256[] memory){
+        return stack;
+    }
+    // your code here
+}
