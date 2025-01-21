bf35ba50d487481098caf3243b37e9b564d0ac76
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-20 21:41:37 -0800

    Create InsertInArray.sol

---
+++ solidity-exercises/contracts/InsertInArray.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract InsertInArray {
+    uint256[10] public arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+    /**
+     * This function is to be used to set/override the value stored at particular index in the array above (named "arr")
+     */
+    function replaceAtIndex(uint256 index, uint256 newValue) public {
+        // your code here
+        arr[index]=newValue;
+    }
+
+    function getFullArray() public view returns (uint256[10] memory){
+        return arr;
+    }
+}
