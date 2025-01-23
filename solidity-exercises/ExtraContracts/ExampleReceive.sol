44c62d8a7cb3dfd7659792e40f9be79c61e810c5
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-23 02:22:26 -0800

    Update ExampleReceive.sol

--- solidity-exercises/ExtraContracts/ExampleReceive.sol
+++ solidity-exercises/ExtraContracts/ExampleReceive.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract TakeMoney {
+    receive() 
+        external 
+        payable {
+
+    }
+
+    function viewBalance() 
+        public 
+        view 
+        returns (uint256) {
+            return address(this).balance;
+    }
+}
+
+contract ForwardMoney {
+    function payMe() 
+        public 
+        payable {
+
+    }
+
+    function sendMoney(address luckyAddress) 
+        public 
+        payable {
+            uint256 myBalance = viewBalance();
+            luckyAddress.call{value: myBalance}("");
+    }
+
+    function viewBalance() 
+        public 
+        view 
+        returns (uint256) {
+            return address(this).balance;
+    }
+}
