ba415e9a915324242ea005e1398cc8232378297a
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-23 02:15:39 -0800

    Update PriceIsRight.sol

--- solidity-exercises/contracts/PriceIsRight.sol
+++ solidity-exercises/contracts/PriceIsRight.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract PriceIsRight {
+    /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
+    function buy() public payable {
+        /// your code here
+        require(msg.value==1 ether,"exactly 1 ether required");
+    }
+}
