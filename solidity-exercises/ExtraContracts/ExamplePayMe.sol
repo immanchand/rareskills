8d5d6c643717b162dcc54ee16fb4699b553c2fd5
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-23 02:08:30 -0800

    Update ExamplePayMe.sol

--- solidity-exercises/ExtraContracts/ExamplePayMe.sol
+++ solidity-exercises/ExtraContracts/ExamplePayMe.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ReceiveEther {
+    function takeMoney() 
+        public 
+         {
+
+    }
+
+    function myBalance() 
+        public 
+        view 
+        returns (uint256) {
+            return address(this).balance;
+    }
+}
+
+contract SendMoney {
+    constructor() 
+        payable {
+
+    }
+
+    function sendMoney(address receiveEtherContract) 
+        public 
+        payable {
+            uint256 amount = myBalance();
+            (bool ok, ) = receiveEtherContract.call{value: amount}(
+                abi.encodeWithSignature("takeMoney()")
+            );
+            require(ok, "transfer failed");
+    }
+
+    function myBalance() 
+        public 
+        view 
+        returns (uint256) {
+            return address(this).balance;
+    }
+}
