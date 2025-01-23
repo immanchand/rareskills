3afe946ff737b8bd0f2e59d8587641b678779373
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-22 20:39:17 -0800

    Update ContractCallingContract.sol

--- solidity-exercises/contracts/ContractCallingContract.sol
+++ solidity-exercises/contracts/ContractCallingContract.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract ExampleContract {
+    function askTheMeaningOfLife(address source)
+        public
+        returns (uint256) {
+            (bool ok, bytes memory result) = source.call(
+                abi.encodeWithSignature("meaningOfLifeAndAllExistence()")
+            );
+            require(ok, "call failed");
+
+            return abi.decode(result, (uint256));
+    }
+}
+
+contract AnotherContract {
+    function meaningOfLifeAndAllExistence()
+        public 
+        pure 
+        returns (uint256) {
+            return 42;
+    }
+}
