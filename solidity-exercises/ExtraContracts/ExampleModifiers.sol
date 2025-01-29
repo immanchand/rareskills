0c47c609c2f2d93da7ec0c3d2f608e21127e7bc4
immanchand <53471578+immanchand@users.noreply.github.com>
2025-01-28 21:49:57 -0800

    Update ExampleModifiers.sol

--- solidity-exercises/ExtraContracts/ExampleModifiers.sol
+++ solidity-exercises/ExtraContracts/ExampleModifiers.sol
@@ -1,1 +1,40 @@
+contract Ownable {
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "onlyOwner");
+        _;
+    }
+
+    function changeOwner(
+        address newOwner
+    ) 
+        public 
+        onlyOwner {
+            owner = newOwner;
+    }
+
+}
+
+contract HoldFunds is Ownable {
+    
+    function withdrawFunds() 
+        public 
+        onlyOwner {
+            (bool ok, ) = owner.call{
+                value: address(this).balance
+            }("");
+            require(ok, "transfer failed");
+    }
+
+    receive() 
+        external 
+        payable {
+
+    }
+}
