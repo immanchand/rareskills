6bfaf16e0f4b1d0740f21d8eda139cf8c7f355a7
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-02 10:44:28 +0400

    Update Immutable.sol

--- solidity-exercises/contracts/Immutable.sol
+++ solidity-exercises/contracts/Immutable.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+/*
+    This exercise assumes you know how to declare immutable variables.
+    1. Make the variable `value` immutable.
+*/
+
+contract ContractImmutable {
+    uint256 immutable public value;
+
+    constructor(uint256 _value) {
+        value = _value;
+    }
+
+}
