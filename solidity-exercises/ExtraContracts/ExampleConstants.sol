fdc3deb143295fc3db5f9410ffdd746b739f5089
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-02 10:48:40 +0400

    Update ExampleConstants.sol

--- solidity-exercises/ExtraContracts/ExampleConstants.sol
+++ solidity-exercises/ExtraContracts/ExampleConstants.sol
@@ -1,1 +1,13 @@
+**contract ExampleERC20 {
 
+    uint256 public constant MAX_SUPPLY = 22_000_000;
+    // erc20 code
+
+    function mint(unint256 amount) external {
+        require(totalSupply() + amount <= MAX_SUPPLY, "max supply exceeded");
+        balanceOf[msg.sender] += amount;
+
+        emit Transfer(address(0), msg.sender, amount);
+    }
+    // rest of the erc20 code
+}**
