f0ee4c75e1b910a344452c317f196f7d631c3210
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 12:26:50 +0400

    Update ERC721_oz.sol

--- solidity-exercises/contracts/ERC721_oz.sol
+++ solidity-exercises/contracts/ERC721_oz.sol
@@ -2,8 +2,9 @@
 pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ICNFT is ERC721("IC ART COLLECTION","ICNFT"){
+contract ICNFT is Ownable(msg.sender), ERC721("IC ART COLLECTION","ICNFT"){
 
     uint256 public constant MAX_SUPPLY = 100;
     uint256 public constant ART_PRICE = 10000000 gwei;
@@ -12,7 +13,10 @@ contract ICNFT is ERC721("IC ART COLLECTION","ICNFT"){
         require(_tokenId < MAX_SUPPLY, "Token out of max supply");
         require(msg.value==ART_PRICE, "Please pay the exact price");
         _safeMint(msg.sender, _tokenId);
+    }
 
+    function withdraw() external onlyOwner {
+        msg.sender.call{value: address(this).balance}("");
     }
 
 }
