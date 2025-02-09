08095bfc0d497bad505d4ef7defcabacced10b24
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 12:00:32 +0400

    Update ERC721_oz.sol

--- solidity-exercises/contracts/ERC721_oz.sol
+++ solidity-exercises/contracts/ERC721_oz.sol
@@ -5,7 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ICNFT is ERC721("IC ART COLLECTION","ICNFT"){
 
+    uint256 public constant MAX_SUPPLY = 100;
+
     function mint(uint256 _tokenId) external {
+        require(_tokenId < MAX_SUPPLY, "Token out of max supply");
         _safeMint(msg.sender, _tokenId);
 
     }
