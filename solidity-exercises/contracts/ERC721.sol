96cafd73e13f8d7bd4d9cd5536b8716e64f264b3
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 08:00:19 +0400

    Update ERC721.sol

--- solidity-exercises/contracts/ERC721.sol
+++ solidity-exercises/contracts/ERC721.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+contract SimpleNFT {
+
+    mapping(uint256=>address) private owners;
+
+    function ownerOf(uint256 _tokenId) external view returns (address){
+
+        return owners[_tokenId];
+    }
+
+}
