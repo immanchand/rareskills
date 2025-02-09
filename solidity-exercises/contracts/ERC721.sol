adc942c6d5f4f848f1b2cffc79e770443edadf88
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 08:48:49 +0400

    Update ERC721.sol

--- solidity-exercises/contracts/ERC721.sol
+++ solidity-exercises/contracts/ERC721.sol
@@ -4,9 +4,12 @@ pragma solidity 0.8.26;
 contract SimpleNFT {
 
     mapping(uint256=>address) private owners;
+    uint256 constant MAX_SUPPLY = 100;
     
     function mint(uint256 _tokenId) external {
 
+        require(owners[_tokenId] == address(0), "Token already minted");
+        require(_tokenId <= MAX_SUPPLY );
         owners[_tokenId] = msg.sender;
     }
 
