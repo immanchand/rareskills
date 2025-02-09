a801f9efc311ca40f9e13f2e1ca116374d4a04a4
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 10:27:24 +0400

    Update ERC721.sol

--- solidity-exercises/contracts/ERC721.sol
+++ solidity-exercises/contracts/ERC721.sol
@@ -9,13 +9,15 @@ contract SimpleNFT {
     mapping(address => mapping(address => bool)) public operators;
     uint256 constant MAX_SUPPLY = 100;
     string constant baseURI = "https://example.com/images/";
-    
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
     
     function mint(uint256 _tokenId) external {
 
         require(owners[_tokenId] == address(0), "Token already minted");
         require(_tokenId < MAX_SUPPLY, "Token outside of MAX SUPPLY" );
         owners[_tokenId] = msg.sender;
+        emit Transfer(address(0), msg.sender, _tokenId);
     }
 
     function ownerOf(uint256 _tokenId) external view returns (address){
@@ -30,6 +32,7 @@ contract SimpleNFT {
 
         owners[_tokenId] = _to;
         operators[_from][msg.sender] = false;
+        emit Transfer(_from, _to, _tokenId);
     }
 
     function tokenURI(uint256 _tokenId) external pure returns (string memory){
@@ -40,5 +43,10 @@ contract SimpleNFT {
     function setApprovalForAll(address _operator, bool _approved) external{
         require(msg.sender != _operator, "Owner and operator should not be the same");
         operators[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool){
+        return operators[_owner][_operator];
     }
 }
