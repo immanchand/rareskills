79c7610076c63884344c91447ba9d6e58cec7847
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 08:04:56 +0400

    Update ERC721.sol

--- solidity-exercises/contracts/ERC721.sol
+++ solidity-exercises/contracts/ERC721.sol
@@ -4,9 +4,14 @@ pragma solidity 0.8.26;
 contract SimpleNFT {
 
     mapping(uint256=>address) private owners;
+    
+    function mint(uint256 _tokenId) external {
+
+        owners[_tokenId] = msg.sender;
+    }
 
     function ownerOf(uint256 _tokenId) external view returns (address){
-
+        require(owners[_tokenId]!=address(0), "Token does not exist");
         return owners[_tokenId];
     }
 
