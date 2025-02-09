2c47c1b9e82a44dbfd9b48e86616ece0c0a6444a
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 14:35:48 +0400

    Update ERC721_oz.sol

--- solidity-exercises/contracts/ERC721_oz.sol
+++ solidity-exercises/contracts/ERC721_oz.sol
@@ -2,12 +2,17 @@
 pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ICNFT is Ownable(msg.sender), ERC721("IC ART COLLECTION","ICNFT"){
+contract ICNFT is ERC721("IC ART COLLECTION","ICNFT"){
 
     uint256 public constant MAX_SUPPLY = 100;
     uint256 public constant ART_PRICE = 10000000 gwei;
+    address public immutable contractOwner;
+
+    constructor() {
+        contractOwner = msg.sender;
+    }
 
     function mint(uint256 _tokenId) external payable{
         require(_tokenId < MAX_SUPPLY, "Token out of max supply");
@@ -15,8 +20,9 @@ contract ICNFT is Ownable(msg.sender), ERC721("IC ART COLLECTION","ICNFT"){
         _safeMint(msg.sender, _tokenId);
     }
 
-    function withdraw() external onlyOwner {
+    function withdraw() external {
        // msg.sender.call{value: address(this).balance}("");
+       require(msg.sender==contractOwner,"Only the artist can withdraw");
        payable(msg.sender).transfer(address(this).balance);
     }
 
