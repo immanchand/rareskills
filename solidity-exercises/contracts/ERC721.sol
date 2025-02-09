79c4aac92c5d3e84152ed5945d703dc939ae6dd1
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-09 09:41:25 +0400

    Update ERC721.sol

--- solidity-exercises/contracts/ERC721.sol
+++ solidity-exercises/contracts/ERC721.sol
@@ -5,6 +5,7 @@ contract SimpleNFT {
 
     mapping(uint256=>address) private owners;
     uint256 constant MAX_SUPPLY = 100;
+    string constant baseURI = "https://example.com/images/";
     
     function mint(uint256 _tokenId) external {
 
@@ -16,6 +17,19 @@ contract SimpleNFT {
     function ownerOf(uint256 _tokenId) external view returns (address){
         require(owners[_tokenId]!=address(0), "Token does not exist");
         return owners[_tokenId];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(owners[_tokenId]!=address(0), "Token does not exist");
+        require(_from==owners[_tokenId], "The from address is not the owner of this token");
+        require(msg.sender==_from, "Only the owner can transfer their token");
+
+        owners[_tokenId]=_to;
+    }
+
+    function tokenURI(uint256 _tokenId) external pure returns (string memory){
+        require(_tokenId<MAX_SUPPLY, "Token outside of MAX SUPPLY");
+        return string(abi.encodePacked(baseURI, _tokenId));
     }
 
 }
