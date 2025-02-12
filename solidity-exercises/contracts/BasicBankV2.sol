fe56362369fc2b0b9a299719268254b6f0c714a8
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-12 11:47:01 +0400

    Update BasicBankV2.sol

--- solidity-exercises/contracts/BasicBankV2.sol
+++ solidity-exercises/contracts/BasicBankV2.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
-contract BasicBank {
-    /// @notice deposit ether into the contract
-    /// @dev it should work properly when called multiple times
-    mapping (address=>uint256) private bankBalances;
+contract BasicBankV2 {
+    /// used to store the balance of users
+    ///     USER    =>  BALANCE
+        mapping (address=>uint256) public balances;
     function addEther() external payable {
-        bankBalances[msg.sender] += msg.value;
+        balances[msg.sender] += msg.value;
     }
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
     function removeEther(uint256 amount) external payable {
-        require(bankBalances[msg.sender]>=amount,"insufficient balance in your account");
+        require(balances[msg.sender]>=amount,"insufficient balance in your account");
         msg.sender.call{value: amount}("");
-        bankBalances[msg.sender] -= amount;
+        balances[msg.sender] -= amount;
     }
 }
