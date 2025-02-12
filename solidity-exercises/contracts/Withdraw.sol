83dc1baa49711cb033c141f192177794523a9341
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-12 14:49:19 +0400

    Update Withdraw.sol

--- solidity-exercises/contracts/Withdraw.sol
+++ solidity-exercises/contracts/Withdraw.sol
@@ -5,7 +5,9 @@ contract Withdraw {
     // @notice make this contract able to receive ether from anyone and anyone can call withdraw below to withdraw all ether from it
     function withdraw() public {
         // your code here
-        msg.sender.call{value: address(this).balance}("");
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Call failed");
+        
     }
     receive() external payable { }
 }
